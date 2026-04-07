77b03152ed41974213271d6a8d4a044c8038c80f
shrimp2845 <goldencheng15@gmail.com>
2026-04-07 16:09:26 +0800

    Refactor ECB encryption and decryption functions

--- ecb.py
+++ ecb.py
@@ -1,42 +1,23 @@
 from des import DES
 import block_utils as b
+import file_utils as f
 
-def read_bytes(key: str, l: int):
-    bfile = open(key, 'rb')
-    data = bfile.read()
-    bfile.close()
-    if len(data) < l:
-        raise ValueError('read_bytes: index overflow')
-    return data[:l]
-    
 def ecb_encrypt(file: str, new_name: str, key: str):
-    key = read_bytes(key, 8)
+    key = f.read_bytes(key, 8)
     cipher = DES(key)
-    bfile = open(file, 'rb')
-    data = bfile.read()
-    bfile.close()
+    data = f.read_bytes(file)
     blocks = b.split_file(b.add_padding(data), 8)
-    c_bytes = b''
-    for i in blocks:
-        c_bytes += cipher.encrypt(i)
-    o_file = open(new_name, 'wb')
-    o_file.write(c_bytes)
-    o_file.close()
+    c_bytes = b''.join(cipher.encrypt(i) for i in blocks)
+    f.write_bytes(new_name, c_bytes)
 
 def ecb_decrypt(file: str, new_name: str, key: str):
-    key = read_bytes(key, 8)
+    key = f.read_bytes(key, 8)
     cipher = DES(key)
-    bfile = open(file, 'rb')
-    data = bfile.read()
-    bfile.close()
+    data = f.read_bytes(file)
     blocks = b.split_file(data, 8)
-    p_bytes = b''
-    for i in blocks:
-        p_bytes += cipher.decrypt(i)
+    p_bytes = b''.join(cipher.decrypt(i) for i in blocks)
     p_bytes = b.remove_padding(p_bytes)
-    o_file = open(new_name, 'wb')
-    o_file.write(p_bytes)
-    o_file.close()
+    f.write_bytes(new_name, p_bytes)
 
 def main():
     f = input('file name:')
