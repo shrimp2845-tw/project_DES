48bf4278ff5d02e14d69f6f2e1071b2c54e60cc9
shrimp2845 <goldencheng15@gmail.com>
2026-04-07 17:01:40 +0800

    Refactor CBC encrypt/decrypt to generate IV internally
    Updated CBC encryption and decryption functions to remove IV parameter and generate IV internally. Modified main function to reflect changes in function calls.

--- cbc.py
+++ cbc.py
@@ -1,44 +1,45 @@
 from des import DES
 import block_utils as b
 import file_utils as f
+import os
 
 def xor(b1: bytes, b2: bytes):
     return bytes(i^j for i, j in zip(b1, b2))
     
-def cbc_encrypt(file: str, new_name: str, key: str, iv: str):
+def cbc_encrypt(file: str, new_name: str, key: str):
     key = f.read_bytes(key, 8)
-    iv = f.read_bytes(iv, 8)
+    iv = os.urandom(8)
     cipher = DES(key)
     data = f.read_bytes(file)
     blocks = b.split_file(b.add_padding(data), 8)
-    c_bytes = b''
+    c_bytes = []
     v = iv
     for i in blocks:
         v = cipher.encrypt(xor(i, v))
-        c_bytes += v
-    f.write_bytes(new_name, c_bytes)
+        c_bytes.append(v)
+    c_bytes = b''.join(c_bytes)
+    f.write_bytes(new_name, iv+c_bytes)
 
-def cbc_decrypt(file: str, new_name: str, key: str, iv: str):
+def cbc_decrypt(file: str, new_name: str, key: str):
     key = f.read_bytes(key, 8)
-    iv = f.read_bytes(iv, 8)
     cipher = DES(key)
     data = f.read_bytes(file)
     blocks = b.split_file(data, 8)
-    va = [iv] + blocks[:-1]
+    va = blocks[:-1]
+    blocks = blocks[1:]
     p_bytes = b''.join(xor(cipher.decrypt(i), j) for i, j in zip(blocks, va))
     p_bytes = b.remove_padding(p_bytes)
     f.write_bytes(new_name, p_bytes)
 
 def main():
-    f = input('file name:')
-    iv = input('initialization vector:')
+    fn = input('file name:')
     k = input('key name:')
     n = input('new name:')
     ed = input('encrypt or decrypt? (e or d)')
     if ed == 'e':
-        cbc_encrypt(f, n, k, iv)
+        cbc_encrypt(fn, n, k)
     else:
-        cbc_decrypt(f, n, k, iv)
+        cbc_decrypt(fn, n, k)
 
 if __name__ == "__main__":
     main()
